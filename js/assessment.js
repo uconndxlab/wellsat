25d652944158d58292cf6ec13f0ada055c5240cf
ryan-cohutt <r13coh@gmail.com>
2024-07-09 16:17:09 -0400

    Header style and fixed accordion function

--- js/assessment.js
+++ js/assessment.js
@@ -1,13 +1,14 @@
-const accordion = document.getElementsByClassName('question-cont');
-let dropdown = document.querySelector('#dropdown-icon')
+const accordions = document.getElementsByClassName('question-cont');
 
-for (i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-    // if (this.classList.contains('active')) {
-    //   // dropdown.innerHTML = '<path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/>';
-    // } else {
-    //   // dropdown.innerHTML = '<path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>';
-    // }
-  })
+for (let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener('click', function () {
+        const content = this.querySelector('.question-content');
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+            content.style.maxHeight = null;
+        } else {
+            this.classList.add('active');
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
