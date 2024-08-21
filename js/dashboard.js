649957bf64e0c43fd9d06da46cdd49572ec86d18
ryan-cohutt <r13coh@gmail.com>
2024-08-21 13:05:43 -0400

    Prelim Pre-Assessment Div

---
+++ js/dashboard.js
@@ -0,0 +1,23 @@
+let create = document.querySelector(".policy-btn")
+let preInfo = document.querySelector(".pre-assess")
+let exit = document.querySelector(".pre-exit")
+
+// preInfo.style.opacity = "0"
+// preInfo.style.display = "none"
+
+create.addEventListener("click", preOpen)
+exit.addEventListener("click", preClose)
+
+function preOpen() {
+    preInfo.style.display = "block"
+    setTimeout(function(){
+        preInfo.style.opacity =  "1"
+    }, 3000)
+}
+
+function preClose() {
+    preInfo.style.opacity =  "0"
+    setTimeout(function(){
+        preInfo.style.display = "none"
+    }, 3000)
+}
