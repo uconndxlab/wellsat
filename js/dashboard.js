e241f3e304a34b26da2bb9547407a36bd510048e
ryan-cohutt <r13coh@gmail.com>
2024-09-02 16:02:27 -0400

    Dashboard Check Boxes

--- js/dashboard.js
+++ js/dashboard.js
@@ -24,4 +24,29 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 300)
     }
 
+    document.querySelectorAll('.checkbox').forEach(function(checkbox) {
+        let checkAll = document.querySelector(".check-all")
+        checkbox.addEventListener('change', function() {
+            const row = this.closest('tr');
+            if (this.checked) {
+                row.style.background = "#70747b";
+                row.style.color = "white";
+            } else {
+                row.style.background = "#ebf0f7";
+                row.style.color = "#70747b";
+            }
+        });
+        checkAll.addEventListener('change', function() {
+            const row = checkbox.closest('tr');
+            if(this.checked) {
+                checkbox.checked = true
+                row.style.background = "#70747b";
+                row.style.color = "white";
+            } else {
+                checkbox.checked = false
+                row.style.background = "#ebf0f7";
+                row.style.color = "#70747b";
+            }
+        })
+    });
 })
