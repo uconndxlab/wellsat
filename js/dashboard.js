8bdb16d78938dc88dc4e8610441bdef07d009778
Natalie L <2018Lacroixn@gmail.com>
2024-09-16 15:43:34 -0400

    responsiveness, contact page, rearranging assessments

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,29 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    let create = document.querySelector(".policy-btn")
-    let preInfo = document.querySelector(".pre-assess")
-    let exit = document.querySelector(".pre-exit")
-
-    preInfo.style.opacity = "0"
-    preInfo.style.display = "none"
-
-    create.addEventListener("click", preOpen)
-    exit.addEventListener("click", preClose)
-
-    function preOpen() {
-        preInfo.style.display = "block"
-        setTimeout(function(){
-            preInfo.style.opacity =  "1"
-        }, 10)
-    }
-
-    function preClose() {
-        preInfo.style.opacity =  "0"
-        setTimeout(function(){
-            preInfo.style.display = "none"
-        }, 300)
-    }
-
     document.querySelectorAll('.checkbox').forEach(function(checkbox) {
         let checkAll = document.querySelector(".check-all")
         checkbox.addEventListener('change', function() {
